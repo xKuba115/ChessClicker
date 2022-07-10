977e1ace7c4ab0fdb5e3dc79f7627932ee3199f2
Kuba Kusiak <thevipgamerpro@gmail.com>
2022-07-10 18:15:31 +0200

    making pawn prices and multiplayers

--- main.py
+++ main.py
@@ -20,10 +20,12 @@ running = True
 white = (255, 255, 255)
 black = (0, 0, 0)
 pawn=pygame.image.load('pawn.png')
-screen.blit(pawn, (50, 50))
+screen.blit(pawn, (50, 400))
 multiply=1
-
-
+pawnPrice = 20
+pawnLevel= 0
+pawnPriceMultiply = 1.3
+i=0
 def DrawText(text, Textcolor, Rectcolor, x, y, fsize):
     font = pygame.font.Font('czcionka.ttf', fsize)
     text = font.render(text, True, Textcolor, Rectcolor)
@@ -47,15 +49,27 @@ while running:
             if click.get_rect().collidepoint(x-(X/2 - 100), y-100):
                 pygame.display.update()
                 check=check+multiply
-            if pawn.get_rect().collidepoint(x-50,y-50):
-                if check>19:
+            if pawn.get_rect().collidepoint(x-50,y-400):
+                if check>=pawnPrice:
+                    pawnLevel=pawnLevel+1
                     multiply=multiply+1
-                    check=check-20
+                    check=check-pawnPrice
+                    if pawnLevel%2==0:
+                        pawnPriceMultiply=pawnPriceMultiply+0.1
+                    pawnPrice=pawnPrice*pawnPriceMultiply
+                    if pawnLevel%10==0 and pawnLevel!=0:
+                        multiply=multiply*1.25
+                         
+                            
+
+                    
+                    pawnPrice=int(pawnPrice)
     
                 
 
         DrawText("You made " + str(f'{check:.2f}') + " checks", black, white, X/2, 50, 20)
-        DrawText("Buy pawn(+1 to click) - 20checks", black,backgroundColour, 110 , 110, 13)
+        DrawText("Buy pawn for  " + str(f'{pawnPrice:.2f}') + " checks(+1 to click power, *1.25 every 10lvl)", black,backgroundColour, 330 , 420, 13)
+        DrawText("You have " + str(f'{pawnLevel:.2f}') + " pawns", black,backgroundColour, 330 , 440, 13)
     pygame.display.update()
 
 
